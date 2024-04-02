8c431aacae5e14aaaed76fb8df9de4ee9199f0f7
Jiwhun Kim <jiwhun.kim@outlook.com>
2024-04-03 01:47:03 +0900

    gralde version up

--- build.gradle.kts
+++ build.gradle.kts
@@ -20,9 +20,9 @@ dependencies {
 	testImplementation("org.junit.jupiter:junit-jupiter")
 	testImplementation("org.assertj:assertj-core:3.25.3")
 
-	testImplementation("io.kotest:kotest-runner-junit5:5.8.0")
-	testImplementation("io.kotest:kotest-assertions-core:5.8.0")
-	testImplementation("io.kotest:kotest-property:5.8.0")
+	testImplementation("io.kotest:kotest-runner-junit5:5.8.1")
+	testImplementation("io.kotest:kotest-assertions-core:5.8.1")
+	testImplementation("io.kotest:kotest-property:5.8.1")
 }
 
 tasks.withType<KotlinCompile> {
